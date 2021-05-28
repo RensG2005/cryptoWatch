e70d78ebfc66932bf5ea3102f26cb48c15998ac0
Rens <gerritsenREN2@gmail.com>
2021-05-28 16:31:45 +0200

    'scrollanimation'

--- pages/index.js
+++ pages/index.js
@@ -6,8 +6,18 @@ import millify from "millify";
 import dateFormat from 'dateformat'
 import Link from 'next/link'
 import ScrollToTop from 'react-scroll-to-top'
+import Aos from 'aos'
+import "aos/dist/aos.css"
+import { useEffect } from "react";
 
 export default function Home({data}) {
+  useEffect(() => {
+    Aos.init({
+      duration: 300,
+      offset: -60
+    })
+  }, [])
+
   return (
     <div>
       <Head>
@@ -15,10 +25,9 @@ export default function Home({data}) {
         <meta name="description" content="A crypto tracker app to watch all the prices of the crypto markets" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       <main>
       <ScrollToTop smooth />
-        <ul className="responsive-table">
+        <ul className="responsive-table"> 
           <li className="table-header">
             <div className="col col-1">#</div>
             <div className="col col-2">Name</div>
@@ -31,47 +40,47 @@ export default function Home({data}) {
         </li>
               {data.map((coin, index) => {
                 return (
-                <li className="table-row" key={coin.id}>
-                    <div className="col col-1" data-label="Index">{index+1}</div>
-                    <div className="col col-2" data-label="Crypto Name">       
-                      <Link href={`coin/${coin.id}`}>
-                        <div className="coinlink" >
-                          <Image src={coin.image.thumb} width={30} height={30} alt={coin.id} />
-                          <h5>{coin.name}<span>{coin.symbol.toUpperCase()}</span></h5>
+                    <li className="table-row" key={coin.id} data-aos="fade-up">
+                        <div className="col col-1" data-label="Index">{index+1}</div>
+                        <div className="col col-2" data-label="Crypto Name">       
+                          <Link href={`coin/${coin.id}`}>
+                            <div className="coinlink" >
+                              <Image src={coin.image.thumb} width={30} height={30} alt={coin.id} />
+                              <h5>{coin.name}<span>{coin.symbol.toUpperCase()}</span></h5>
+                            </div>
+                          </Link> 
                         </div>
-                      </Link> 
-                    </div>
-              <div className="col col-3" data-label="Price: ">
-                ${coin.market_data.current_price.usd}
-              </div>
-              <div className={coin.market_data.price_change_percentage_24h >= 0 ? "col col-4 green" : "col col-4 red"} data-label="24h %: ">
-                {coin.market_data.price_change_percentage_24h.toFixed(2)}%
-              </div>
+                  <div className="col col-3" data-label="Price: ">
+                    ${coin.market_data.current_price.usd}
+                  </div>
+                  <div className={coin.market_data.price_change_percentage_24h >= 0 ? "col col-4 green" : "col col-4 red"} data-label="24h %: ">
+                    {coin.market_data.price_change_percentage_24h.toFixed(2)}%
+                  </div>
 
-            <div className={coin.market_data.price_change_percentage_7d >= 0 ? "col col-5 green" : "col col-5 red"} data-label="Payment Status">
-                {coin.market_data.price_change_percentage_7d.toFixed(2)}%
-            </div>
-            <div className="col col-6" data-label="Market Cap">
-                ${millify(coin.market_data.market_cap.usd, {
-                  precision: 3
-                })}
-            </div>
-            <div className="col col-7" data-label="Total Volume">
-              {millify(coin.market_data.total_volume.usd, {
-                precision: 3
-              })}
-            </div>
-            <div className="col col-8" data-label="Last Updated">
-                {dateFormat(coin.last_updated,  "mmmm dS, yyyy, h:MM:ss TT").toString()}
-            </div>
-            </li>
+                <div className={coin.market_data.price_change_percentage_7d >= 0 ? "col col-5 green" : "col col-5 red"} data-label="Payment Status">
+                    {coin.market_data.price_change_percentage_7d.toFixed(2)}%
+                </div>
+                <div className="col col-6" data-label="Market Cap">
+                    ${millify(coin.market_data.market_cap.usd, {
+                      precision: 3
+                    })}
+                </div>
+                <div className="col col-7" data-label="Total Volume">
+                  {millify(coin.market_data.total_volume.usd, {
+                    precision: 3
+                  })}
+                </div>
+                <div className="col col-8" data-label="Last Updated">
+                    {dateFormat(coin.last_updated,  "mmmm dS, yyyy, h:MM:ss TT").toString()}
+                </div>
+                </li>
           )}
           )} 
           </ul>
       </main> 
 
       <footer>
-        <p>made by Rens Gerritsen</p>
+        <p>Made by Rens Gerritsen || <a>https://github.com/RensG2005/cryptoWatch</a></p>
       </footer>
     </div>
   )
